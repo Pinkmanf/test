c6427ec321bd8f9a1b57152b00287695521c449c
Pinkman <2538424511@qq.com>
2023-04-10 19:35:45 +0800

    test

---
+++ Study/IdeaProjects/test0/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int i=0,a=0;
+        for (int j = 0; j < 897; j++) {
+            for (int k = 0; k <978897 ; k++) {
+                i++;
+            }
+        }
+        for (int j = 0; j < 897; j++) {
+            for (int k = 0; k <978897 ; k++) {
+                a++;
+            }
+        }
+        System.out.println("hello,world!"+i+a);
+    }
+}
